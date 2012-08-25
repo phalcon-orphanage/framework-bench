115c78da5a35278f7cfcef33b9745ae69cfa326b
resurtm <resurtm@gmail.com>
2012-08-25 20:38:20 +0600

    Yii updated from 1.1.10 to the 1.1.12.

--- yii/index.php
+++ yii/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii-1.1.10.r3566/framework/yii.php';
+$yii=dirname(__FILE__).'/../yii-1.1.12.b600af/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
