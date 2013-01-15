8f54494012d6faec7b3c44e8bcf788ed091f210a
resurtm <resurtm@gmail.com>
2013-01-15 10:18:15 +0600

    Yii benchmark refinements.

--- helloworld/yii/protected/controllers/SayController.php
+++ helloworld/yii/protected/controllers/SayController.php
@@ -2,16 +2,10 @@
 
 class SayController extends CController
 {
-	public $layout='//layouts/column1';
+	public $layout=false;
 
-	/**
-	 * This is the default 'index' action that is invoked
-	 * when an action is not explicitly requested by users.
-	 */
 	public function actionHello()
 	{
-		// renders the view file 'protected/views/hello/index.php'
-		// using the default layout 'protected/views/layouts/main.php'
 		$this->render('hello');
 	}
 
